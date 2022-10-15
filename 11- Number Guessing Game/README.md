10d3f274dc45597a337d064d963b6ba421486d74
Zhuo <hudirybw@gmail.com>
2022-10-15 09:04:49 +0200

    feat: project done

--- 11- Number Guessing Game/README.md
+++ 11- Number Guessing Game/README.md
@@ -1,7 +1,21 @@
 If that username has been used before, it should print Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses., with <username> being a users name from the database, <games_played> being the total number of games that user has played, and <best_game> being the fewest number of guesses it took that user to win the game
 
-username 
-games_played
+name 
+times
 best_game
 
 
+1 create database
+2 set column's detail
+3 write script
+
+
+    Column    |       Type        | Collation | Nullable |                Default                 
+--------------+-------------------+-----------+----------+----------------------------------------
+ user_id      | integer           |           | not null | nextval('users_user_id_seq'::regclass)
+ name         | character varying |           |          | 
+ games_played | integer           |           |          | 0
+ best_game    | integer           |           |          | 
+Indexes:
+    "users_pkey" PRIMARY KEY, btree (user_id)
+    "users_name_key" UNIQUE CONSTRAINT, btree (name)
--- 11- Number Guessing Game/number_guess.sh
+++ 11- Number Guessing Game/number_guess.sh
@@ -1,1 +1,80 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --tuples-only  -c"
+
+# create random number
+RAN_NUM=$(( RANDOM % 1000 + 1 ))
+ echo $RAN_NUM
+
+echo -e "\nEnter your username:"
+read USERNAME
+
+RESULT=$($PSQL "SELECT games_played, best_game FROM users WHERE name='$USERNAME'")
+
+# Init GAMES and BEST 
+GAMES=0
+BEST=999999
+
+if [[ -z $RESULT ]]
+then
+  # add new user to db
+  NEW_USER=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME');")
+  # greeting new user
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+  
+else
+  # there is this user in db
+  # read data into variables
+  echo $RESULT | while read GAMES_PLAYED BAR BEST_GAME 
+  do
+  # greeting user
+  echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  # updata variable
+  GAMES=$GAMES_PLAYED
+  BEST=$BEST_GAME
+  done
+fi 
+
+# GAME START!
+echo -e "\nGuess the secret number between 1 and 1000:"
+read INPUT
+
+# check if input valid
+while [[ ! $INPUT =~ ^[0-9]+$ ]]
+do
+  echo "That is not an integer, guess again:"
+  read INPUT
+done
+
+GUESS=1
+while [[ $INPUT != $RAN_NUM ]]
+do
+if [[ $INPUT > $RAN_NUM ]]
+then
+  echo "It's higher than that, guess again:"
+else
+  echo "It's lower than that, guess again:"
+fi
+read INPUT
+
+# check if input valid
+while [[ ! $INPUT =~ ^[0-9]+$ ]]
+do
+  echo "That is not an integer, guess again:"
+  read INPUT
+done
+
+(( GUESS++ ))
+done
+# GAME OVER!
+echo -e "\nYou guessed it in $GUESS tries. The secret number was $RAN_NUM. Nice job!"
+
+# update db
+(( GAMES++ ))
+UPDATE=$($PSQL "UPDATE users SET games_played=$GAMES WHERE name='$USERNAME';")
+
+# check is it the best guesses
+if [[ $GUESS < $BEST ]]
+then
+  UPDATE=$($PSQL "UPDATE users SET best_game=$GUESS WHERE name='$USERNAME';")
+fi
