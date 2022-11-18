0c9f9b056ab653f274608744995e4f8764863519
Zhuo <hudirybw@gmail.com>
2022-11-18 20:39:57 +0100

    feat: back to js and stop at check row placement

---
+++ boilerplate-project-sudoku-solver/controllers/sudoku-solver.js
@@ -0,0 +1,74 @@
+class SudokuSolver {
+  constructor(){
+    this.basicArr = [1,2,3,4,5,6,7,8,9]
+  }
+
+  checkBasic(puzzleString) { // check them contain 1-9
+    let res = true
+    this.basicArr.forEach(n =>{
+      if (puzzleString.split('').filter(x=> +x==n).length != 1) res =false
+    })
+    return res
+  }
+
+  validate(puzzleString) { // check the whole puzzle
+    const regex = /[1-9.]/
+    if (puzzleString.length == 81 && puzzleString.split('').every(x=>regex.test(x))) return true
+    return false
+  }
+
+  checkRowPlacement(puzzleString, row, column, value) {
+    let rowArr = [];
+    for(let i = 0 ; i < puzzleString.length; i++) {
+      rowArr.push(puzzleString[i])
+      // when there is a row, check it, if pass, empty it and go on, else return false
+      if(i % 8 == 0 && i != 0) {
+        if(!this.checkBasic(rowArr.join(''))){
+          console.log(rowArr)
+          return false;
+        }
+        rowArr = []
+      };
+    }
+    return true
+  }
+
+  checkColPlacement(puzzleString , row, column, value) {
+    const columns = 9;
+    for (let i = 1; i <= columns; i++){
+      let columnArr = [puzzleString[i-1]];
+      for (let j = i * 9 ; j < 81; j+=9){
+        columnArr.push(puzzleString[j])
+      };
+      if(!this.checkBasic(columnArr.join(''))) 
+        return false
+    }
+    return true
+  }
+
+  checkRegionPlacement(puzzleString , row, column, value) {
+    const RegionRow = {
+      a : 0,
+      b : 1,
+      c : 2
+    }
+    for(let i = 1; i <= 9; i++){ // there are 9 regions
+      let regionArr = []
+      for(let j = 0; j <= 18; j += 9) {
+        regionArr.push(puzzleString[RegionRow.a + i * j])
+        regionArr.push(puzzleString[RegionRow.b + i * j])
+        regionArr.push(puzzleString[RegionRow.c + i * j])
+      }
+      if(!this.checkBasic(regionArr.join(''))) 
+      return false
+    }
+    return true
+  }
+
+  solve(puzzleString) {
+    
+  }
+}
+
+module.exports = SudokuSolver;
+
--- boilerplate-project-sudoku-solver/tests/1_unit-tests.js
+++ boilerplate-project-sudoku-solver/tests/1_unit-tests.js
@@ -2,8 +2,50 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
+const {puzzlesAndSolutions} = require('../controllers/puzzle-strings')
+const solutionsArr = puzzlesAndSolutions.map(x=>x[1])
 let solver;
 
 suite('Unit Tests', () => {
+    test('valid puzzle', done => {
+        assert.isOk(new Solver().validate('1'.repeat(81)))
+        assert.isOk(new Solver().validate('9'.repeat(81)))
+        assert.isOk(new Solver().validate('.'.repeat(81)))
+        done()
+    })
+
+    test('invalid characters', done => {
+        assert.isNotOk(new Solver().validate('a'.repeat(81)))
+        done()
+    })
+
+    test('not 81 characters', done => {
+        assert.isNotOk(new Solver().validate('1'.repeat(22)))
+        assert.isNotOk(new Solver().validate('9'.repeat(66)))
+        assert.isNotOk(new Solver().validate('.'.repeat(80)))
+        done()
+    })
+
+    test('basic check', done => {
+       assert.isOk(new Solver().checkBasic([
+        '1', '3', '5',
+        '7', '6', '2',
+        '9', '8', '4'
+      ].join('')))
+      assert.isNotOk(new Solver().checkBasic([
+        '1', '3', '5',
+        '7', '6', '2',
+        '9', '9', '4'
+      ].join('')))
+       done()
+    })
+
+    test('valid row placement', done => {
+        solutionsArr.forEach(x=> {
+            console.log(x)
+            assert.isOk(new Solver().checkRowPlacement(x))
+        })
+        done()
+    })
 
 });
