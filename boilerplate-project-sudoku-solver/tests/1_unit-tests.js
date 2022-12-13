831a48e52132c6df341444bfef7c984a01c44b00
hudir <hudirybw@gmail.com>
2022-12-13 07:17:13 +0100

    add two unit test

--- boilerplate-project-sudoku-solver/tests/1_unit-tests.js
+++ boilerplate-project-sudoku-solver/tests/1_unit-tests.js
@@ -1,86 +1,129 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-const {puzzlesAndSolutions} = require('../controllers/puzzle-strings')
-const solutionsArr = puzzlesAndSolutions.map(x=>x[1])
+const Solver = require("../controllers/sudoku-solver.js");
+const { puzzlesAndSolutions } = require("../controllers/puzzle-strings");
+const solutionsArr = puzzlesAndSolutions.map((x) => x[1]);
 let solver;
 
-suite('Unit Tests', () => {
-    test('valid puzzle', done => {
-        assert.isTrue((new Solver().validate('1'.repeat(81))).validate)
-        assert.isTrue((new Solver().validate('9'.repeat(81))).validate)
-        assert.isTrue((new Solver().validate('.'.repeat(81))).validate)
-        done()
-    })
+suite("Unit Tests", () => {
+  test("valid puzzle", (done) => {
+    assert.isTrue(new Solver().validate("1".repeat(81)).validate);
+    assert.isTrue(new Solver().validate("9".repeat(81)).validate);
+    assert.isTrue(new Solver().validate(".".repeat(81)).validate);
+    done();
+  });
 
-    test('invalid characters', done => {
-        assert.equal(new Solver().validate('a'.repeat(81)).error, 'Invalid characters in puzzle')
-        done()
-    })
+  test("invalid characters", (done) => {
+    assert.equal(
+      new Solver().validate("a".repeat(81)).error,
+      "Invalid characters in puzzle"
+    );
+    done();
+  });
 
-    test('not 81 characters', done => {
-        assert.equal(new Solver().validate('1'.repeat(22)).error, 'Expected puzzle to be 81 characters long')
-        assert.equal(new Solver().validate('9'.repeat(66)).error, 'Expected puzzle to be 81 characters long')
-        assert.equal(new Solver().validate('.'.repeat(80)).error, 'Expected puzzle to be 81 characters long')
-        done()
-    })
+  test("not 81 characters", (done) => {
+    assert.equal(
+      new Solver().validate("1".repeat(22)).error,
+      "Expected puzzle to be 81 characters long"
+    );
+    assert.equal(
+      new Solver().validate("9".repeat(66)).error,
+      "Expected puzzle to be 81 characters long"
+    );
+    assert.equal(
+      new Solver().validate(".".repeat(80)).error,
+      "Expected puzzle to be 81 characters long"
+    );
+    done();
+  });
 
-    test('basic check', done => {
-       assert.isOk(new Solver().checkBasic([
-        '1', '3', '5',
-        '7', '6', '2',
-        '9', '8', '4'
-      ].join('')))
-      assert.isNotOk(new Solver().checkBasic([
-        '1', '3', '5',
-        '7', '6', '2',
-        '9', '9', '4'
-      ].join('')))
-       done()
-    })
+  test("basic check", (done) => {
+    assert.isOk(
+      new Solver().checkBasic(
+        ["1", "3", "5", "7", "6", "2", "9", "8", "4"].join("")
+      )
+    );
+    assert.isNotOk(
+      new Solver().checkBasic(
+        ["1", "3", "5", "7", "6", "2", "9", "9", "4"].join("")
+      )
+    );
+    done();
+  });
 
-    test('valid row placement', done => {
-        solutionsArr.forEach(x=> {
-            assert.isOk(new Solver().checkRowPlacement(x))
-        })
-        done()
-    })
+  test("valid row placement", (done) => {
+    solutionsArr.forEach((x) => {
+      assert.isOk(new Solver().checkRowPlacement(x));
+    });
+    done();
+  });
 
-    test('invalid row placement', done => {   
-      assert.isNotOk(new Solver().checkRowPlacement("887549163531672894649831527496157382218396475753284916962415738185763249374928651"))
-      done()
-    })
+  test("invalid row placement", (done) => {
+    assert.isNotOk(
+      new Solver().checkRowPlacement(
+        "887549163531672894649831527496157382218396475753284916962415738185763249374928651"
+      )
+    );
+    done();
+  });
 
-    test('valid column placement', done => {
-        solutionsArr.forEach(x=> {
-            assert.isOk(new Solver().checkColPlacement(x))
-        })
-        done()
-    })
+  test("valid column placement", (done) => {
+    solutionsArr.forEach((x) => {
+      assert.isOk(new Solver().checkColPlacement(x));
+    });
+    done();
+  });
 
-    test('invalid column placement', done => {   
-      assert.isNotOk(new Solver().checkColPlacement("987549163531672894649831527496157382218396475753284916962415738185763249374928651"))
-      done()
-    })
+  test("invalid column placement", (done) => {
+    assert.isNotOk(
+      new Solver().checkColPlacement(
+        "987549163531672894649831527496157382218396475753284916962415738185763249374928651"
+      )
+    );
+    done();
+  });
 
-    test('valid region (3x3 grid) placement', done => {
-        solutionsArr.forEach(x=> {
-            assert.isOk(new Solver().checkRegionPlacement(x))
-        })
-        done()
-    })
+  test("valid region (3x3 grid) placement", (done) => {
+    solutionsArr.forEach((x) => {
+      assert.isOk(new Solver().checkRegionPlacement(x));
+    });
+    done();
+  });
 
-    test('invalid region (3x3 grid) placement', done => {   
-      assert.isNotOk(new Solver().checkRegionPlacement("987549163531672894649831527496157382218396475753284916962415738185763249374928651"))
-      done()
-    })
+  test("invalid region (3x3 grid) placement", (done) => {
+    assert.isNotOk(
+      new Solver().checkRegionPlacement(
+        "987549163531672894649831527496157382218396475753284916962415738185763249374928651"
+      )
+    );
+    done();
+  });
 
-    test('sudoku solving, row', done => {
-        puzzlesAndSolutions.forEach(el => {
-            assert.equal( new Solver().solve(el[0]), el[1])
-        })    
-        done()
-    })
+  const sudokuExpValid =
+    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
 
+  const sudokuExpInvalid =
+    "9.9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+
+  const solver = new Solver();
+
+  test("Valid puzzle strings pass the solver", (done) => {
+    assert.isOk(solver.solve(sudokuExpValid));
+    assert.isString(solver.solve(sudokuExpValid));
+    assert.lengthOf(solver.solve(sudokuExpValid), 81 );
+  done();
+});
+
+  test("Invalid puzzle strings fail the solver", (done) => {
+    assert.isFalse(solver.solve(sudokuExpInvalid));
+  done();
+});
+
+  test("sudoku solving", (done) => {
+    puzzlesAndSolutions.forEach((el) => {
+      assert.equal(new Solver().solve(el[0]), el[1]);
+    });
+    done();
+  });
 });
