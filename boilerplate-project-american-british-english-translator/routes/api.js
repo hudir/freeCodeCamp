07db4131f53a6be77139e1c1c1256d7d2af7d279
hudir <hudirybw@gmail.com>
2022-12-25 11:38:16 +0100

    functional test done

--- boilerplate-project-american-british-english-translator/routes/api.js
+++ boilerplate-project-american-british-english-translator/routes/api.js
@@ -8,16 +8,17 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => { 
+      // console.log(req.body, "request",!req.body.hasOwnProperty('text') || !req.body.hasOwnProperty('locale'))
       if(req.body.hasOwnProperty('text') && req.body.text == "") {
         return res.json({ error: 'No text to translate' })
-      } else if(!req.body.hasOwnProperty('text') || req.body.hasOwnProperty('locale')) {
+      } else if(!req.body.hasOwnProperty('text') || !req.body.hasOwnProperty('locale')) {
         return res.json({ error: 'Required field(s) missing' })
       } 
 
-      if(req.body.locale != "american-to-british" || req.body.locale != "british-to-american") {
+      if(req.body.locale != "american-to-british" && req.body.locale != "british-to-american") {
         return res.json({ error: 'Invalid value for locale field' })
       }
 
-      res.json(translator.trans(req.body.text, req.body.locale))
+      res.json({translation: translator.trans(req.body.text, req.body.locale)})
     });
 };
--- boilerplate-project-american-british-english-translator/tests/2_functional-tests.js
+++ boilerplate-project-american-british-english-translator/tests/2_functional-tests.js
@@ -8,5 +8,75 @@ chai.use(chaiHttp);
 let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
+    // // Translation with text and locale fields: POST request to /api/translate
+    test("Translation with text and locale fields: POST request to /api/translate", done => {
+        chai.request(server)
+            .post('/api/translate')
+            .send({
+                text: "Have you met Mrs Kalyani?",
+                locale: "british-to-american"
+            })
+            .end((err, res) => {
+                assert.equal(res.body.translation, `Have you met <span class="highlight">Mrs.</span> Kalyani?`)
+            })
+        done()
+    })
+
+    // // // Translation with text and invalid locale field: POST request to /api/translate
+    test("Translation with text and invalid locale field: POST request to /api/translate", done => {
+        chai.request(server)
+            .post('/api/translate')
+            .send({
+                text: "Have you met Mrs Kalyani?",
+                locale: "111"
+            })
+            .end((err, res) => {
+                assert.equal(res.body.error, 'Invalid value for locale field')
+            })
+        done()
+    })
+    // // // Translation with missing text field: POST request to /api/translate
+    test("Translation with missing text field: POST request to /api/translate", done => {
+        chai.request(server)
+            .post('/api/translate')
+            .send({   locale: "british-to-american"  })
+            .end((err, res) => {
+                assert.equal(res.body.error, 'Required field(s) missing')
+            })
+        done()
+    })
+    // // // Translation with missing locale field: POST request to /api/translate
+    test("Translation with missing locale field: POST request to /api/translate", done => {
+        chai.request(server)
+            .post('/api/translate')
+            .send({ text: "Have you met Mrs Kalyani?" })
+            .end((err, res) => {
+                assert.equal(res.body.error, 'Required field(s) missing')
+            })
+        done()
+    })
+    // // // Translation with empty text: POST request to /api/translate
+    test("Translation with empty text: POST request to /api/translate", done => {
+        chai.request(server)
+            .post('/api/translate')
+            .send({ text: ""})
+            .end((err, res) => {
+                assert.equal(res.body.error, 'No text to translate' )
+            })
+        done()
+    })
+    // // // Translation with text that needs no translation: POST request to /api/translate
+    test("Translation with text that needs no translation: POST request to /api/translate", done => {
+        chai.request(server)
+            .post('/api/translate')
+            .send({
+                text: "Mangoes are my favorite fruit",
+                locale: "american-to-british"
+            })
+            .end((err, res) => {
+                assert.equal(res.body.translation, `Mangoes are my <span class="highlight">favourite</span> fruit`)
+            })
+        done()
+    })
 
 });
