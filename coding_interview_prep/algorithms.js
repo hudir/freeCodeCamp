1317c6e6a98d7b22a9ab17689ac575e4fd7344e4
Zhuo <hudirybw@gmail.com>
2023-01-08 19:38:46 +0100

    lustig

--- coding_interview_prep/algorithms.js
+++ coding_interview_prep/algorithms.js
@@ -25,3 +25,59 @@ function sym(args) {
   }
   
   sym([1, 2, 3], [5, 2, 1, 4]);
+
+
+
+  function updateInventory(arr1, arr2) {
+    let obj1 = convertArrToObj(arr1);
+    let obj2 = convertArrToObj(arr2);
+    obj1 = addToResult(obj2, obj1);
+    let resArr = []
+    for(let key in obj1) {
+        const temArr = [obj1[key], key];
+        resArr.push(temArr)
+    }
+    resArr = resArr.sort((a,b) => {
+        const c1 = a[1].toUpperCase()
+        const c2 = b[1].toUpperCase()
+        if(c1 > c2) return 1
+        else if (c1 < c2) return -1
+        else return 0
+
+    })
+    return resArr;
+
+    function convertArrToObj(arr) {
+        return arr.reduce((acc,el)=>{
+            acc[el[1]] = +el[0];
+            return acc
+        }, {})
+    }
+    function addToResult(from, to) {
+        for(let key in from){
+            if(to.hasOwnProperty(key)){
+                to[key] += from[key] 
+            } else {
+                to[key] = from[key] 
+            }
+        }
+        return to;
+    }
+}
+
+// Example inventory lists
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
