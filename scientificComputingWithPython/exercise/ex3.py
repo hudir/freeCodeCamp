e78684809461e1e5476ce1c59798fed13df8eeb7
Zhuo <hudirybw@gmail.com>
2023-02-09 15:36:31 +0100

    nice ex3.2

--- scientificComputingWithPython/exercise/ex3.py
+++ scientificComputingWithPython/exercise/ex3.py
@@ -1,16 +1,21 @@
 hours = input("Enter Hours: ")
 rate = input("Enter Rate: ")
 
-floatHours = float(hours)
-floatRate = float(rate)
+try: 
+    floatHours = float(hours)
+    floatRate = float(rate)
 
-if floatHours > 40: 
-    print('Overtim')
-    reg = floatHours * floatRate
-    otp = (floatHours - 40) * floatRate * 0.5
-    xp = reg + otp
-else: 
-    print('Regular')
-    xp = floatHours * floatRate
+    if floatHours > 40: 
+        print('Overtim')
+        reg = floatHours * floatRate
+        otp = (floatHours - 40) * floatRate * 0.5
+        xp = reg + otp
+    else: 
+        print('Regular')
+        xp = floatHours * floatRate
 
-print("Pay:", xp)
+    print("Pay:", xp)
+
+except:
+    print('Error, please enter numberic input')
+    # quit()
