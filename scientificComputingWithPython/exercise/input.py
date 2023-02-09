0c1c0e96c6efc0dbba2ec77b16414f3b3f29c671
Zhuo <hudirybw@gmail.com>
2023-02-09 10:34:40 +0100

    ex

---
+++ scientificComputingWithPython/exercise/input.py
@@ -0,0 +1,15 @@
+# exercise 2
+print("Hello Wourld")
+
+nzt = input("Enter your name: ")
+
+print("Hello", nzt)
+
+# ex 3
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+print("Pay:", float(hours) * float(rate))
+
+print(type(hours))
+
+print(type(int(hours)))
