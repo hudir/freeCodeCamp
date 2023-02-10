f8d5da20877266aa2324faeb8349cc24c017a0be
Zhuo <hudirybw@gmail.com>
2023-02-10 11:48:11 +0100

    loooooping

---
+++ scientificComputingWithPython/loopAndIterations.py
@@ -0,0 +1,23 @@
+# n = 5 # itration variable
+# while n > 0:
+#     print(n)
+#     n = n - 1
+# print('Blastoff')
+# print(n)
+
+# while True:
+#     line = input('> ')
+#     if line == 'done' :
+#         break
+#     print(line)
+# print('Done!')
+
+
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line == 'done' :
+        break
+    print(line)
+print('Done!')
