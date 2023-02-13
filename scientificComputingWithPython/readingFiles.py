712ab302e6c29b4bb2629f2d0bd47f56c43fad22
Zhuo <hudirybw@gmail.com>
2023-02-13 14:22:41 +0100

    back slash n  is a single char

---
+++ scientificComputingWithPython/readingFiles.py
@@ -0,0 +1,8 @@
+# open(fileName, mode)
+file1 = open('strings.py')
+print(file1) # <_io.TextIOWrapper name='strings.py' mode='r' encoding='UTF-8'>
+
+# \n is one charactor
+stuff = 'X\nY'
+print(stuff)
+print(len(stuff)) # 3
