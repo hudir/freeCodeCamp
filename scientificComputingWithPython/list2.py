9f098c2b589a5be867487c3d61b2a2142b8a7521
Zhuo <hudirybw@gmail.com>
2023-02-15 13:26:12 +0100

    list part 2

--- scientificComputingWithPython/list2.py
+++ scientificComputingWithPython/list2.py
@@ -28,3 +28,24 @@ print(9 in some)
 print(15 in some)
 print(2 not in some)
 
+friends = ['Joseph', 'Glenn', 'Sally']
+friends.sort()
+print(friends) # ['Glenn', 'Joseph', 'Sally']
+
+nums = [3, 41, 12, 9, 74, 15]
+print(len(nums))
+print(max(nums))
+print(min(nums))
+print(sum(nums))
+print(sum(nums) / len(nums))
+
+
+numlist = list()
+while True:
+    inp = input('Enter a number: ')
+    if inp == 'done' : break
+    value = float(inp)
+    numlist.append(value)
+
+average = sum(numlist) / len(numlist)
+print('Average: ', average)
