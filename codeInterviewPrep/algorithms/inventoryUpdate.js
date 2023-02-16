fe798932cad7faa0ff01eace774d68b9e11261a5
Zhuo <hudirybw@gmail.com>
2023-02-16 14:48:06 +0100

    fun to do

---
+++ codeInterviewPrep/algorithms/inventoryUpdate.js
@@ -0,0 +1,56 @@
+// comvert both array to objet
+// {
+//     "Bowling Ball": 1
+// }
+// itration though the object and add or create new key
+// convertback to 2D array
+// sort
+
+function updateInventory(arr1, arr2) {
+    const curInvObj = from2DArrToObj(arr1)
+    const newInvObj = from2DArrToObj(arr2)
+
+    for(let key in newInvObj) {
+        if(curInvObj.hasOwnProperty(key)){
+            curInvObj[key] = +curInvObj[key] + +newInvObj[key]
+        } else curInvObj[key] = +newInvObj[key]
+    }
+    let arr2d = []
+    for(let key in curInvObj) {
+        const arr1d = [curInvObj[key], key]
+        arr2d.push(arr1d)
+    }
+
+    arr2d.sort((a,b)=>{
+        if(a[1][0] < b[1][0]) return -1
+        if(a[1][0] > b[1][0]) return 1
+        return 0
+    })
+
+    return arr2d;
+
+    function from2DArrToObj(arr){
+        return arr.reduce((acc,x)=> {
+            acc[x[1]] = x[0]
+            return acc 
+        }, {})
+
+    }
+}
+
+// Example inventory lists
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
