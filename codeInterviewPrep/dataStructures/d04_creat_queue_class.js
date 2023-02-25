6f17a02c0b96d1014c070dbd47561d341820adeb
Zhuo <hudirybw@gmail.com>
2023-02-25 13:34:56 +0100

    quequeque

--- codeInterviewPrep/dataStructures/d04_creat_queue_class.js
+++ codeInterviewPrep/dataStructures/d04_creat_queue_class.js
@@ -12,9 +12,31 @@ function Queue() {
     }
 
     this.dequeue = function() {
-        first = collection[0]
+        const first = collection[0]
         collection = collection.filter((x,i)=> i != 0)
         return first
     }
+
+    this.front = function() {
+        return collection[0]
+    }
+
+    this.size = function() {
+        return collection.length
+    }
+
+    this.isEmpty = function() {
+        return collection.length === 0 ? true : false
+    }
    
 }
+
+const instance = new Queue()
+instance.enqueue(1, 2, 3, 4)
+instance.print()
+
+instance.enqueue(5)
+instance.print()
+
+console.log(instance.dequeue());
+instance.print()
