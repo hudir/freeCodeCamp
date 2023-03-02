96c20e4c3dca85b8d18c0e1941737a137621c5fa
Zhuo <hudirybw@gmail.com>
2023-03-02 17:04:38 +0100

    done

--- scientificComputingWithPython/exercise/ex10/ex3.py
+++ scientificComputingWithPython/exercise/ex10/ex3.py
@@ -16,4 +16,36 @@ for line in input:
         if letter in abc:
             obj[letter] = obj[letter] + 1
 
-print( sorted([ (v,k) for k,v in obj.items()], reverse=True))
+totol = sum([ v for v in obj.values()])
+print(totol)
+
+result =  sorted([ (v,k) for k,v in obj.items()], reverse=True)
+for tp in result:
+    print(tp[1], round(tp[0]/totol *1000) / 10 , '%')
+
+# e 9.3 %
+# a 8.9 %
+# i 7.7 %
+# o 7.1 %
+# r 6.9 %
+# t 6.9 %
+# s 6.4 %
+# u 5.3 %
+# c 5.3 %
+# n 4.4 %
+# p 4.3 %
+# m 4.2 %
+# d 3.4 %
+# l 3.1 %
+# h 2.4 %
+# f 2.1 %
+# k 2.0 %
+# b 1.9 %
+# g 1.8 %
+# v 1.7 %
+# j 1.6 %
+# y 1.1 %
+# w 1.0 %
+# x 0.8 %
+# z 0.1 %
+# q 0.1 %
