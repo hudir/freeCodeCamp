e421a55af3c8805c70e72e3952a0387e99d14981
Zhuo <hudirybw@gmail.com>
2023-03-06 15:51:32 +0100

    works

--- scientificComputingWithPython/socket_web_browser.py
+++ scientificComputingWithPython/socket_web_browser.py
@@ -10,7 +10,7 @@ import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
@@ -19,3 +19,39 @@ while True:
         break
     print(data.decode())
 mysock.close()
+
+
+# import socket
+# import time
+
+# HOST = 'data.pr4e.org'
+# PORT = 80
+# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# mysock.connect((HOST, PORT))
+# mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\r\n\r\n')
+# count = 0
+# picture = b""
+
+# while True:
+#     data = mysock.recv(5120)
+#     if (len(data) < 1): break
+#     time.sleep(0.25)
+#     count = count + len(data)
+#     print(len(data), count)
+#     picture = picture + data
+
+# mysock.close()
+
+# # Look for the end of the header (2 CRLF)
+# pos = picture.find(b"\r\n\r\n")
+# print('Header length', pos)
+# print(picture[:pos].decode())
+
+# # Skip past the header and save the picture data
+# picture = picture[pos+4:]
+# fhand = open("stuff.jpg", "wb")
+# fhand.write(picture)
+# fhand.close()
+
+# # Code: http://www.py4e.com/code3/urljpeg.py
+# # Or select Download from this trinket's left-hand menu
--- scientificComputingWithPython/web_scraping_with_python.py
+++ scientificComputingWithPython/web_scraping_with_python.py
@@ -1,10 +1,20 @@
 import urllib.request, urllib.parse,  urllib.error 
 from bs4 import BeautifulSoup
+import ssl
 
-url = input('Enter Url - ')
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = 'http://www.dr-chuck.com/page1.htm'
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 tags = soup('a')
 for tag in tags:
-    print(tag.get('href', None))
+      # Look at the parts of a tag
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
