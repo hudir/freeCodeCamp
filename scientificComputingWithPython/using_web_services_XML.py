699345c0b5ffd1b8c159f44faebc05bd48d67504
Zhuo <hudirybw@gmail.com>
2023-03-07 15:21:42 +0100

    XSD

--- scientificComputingWithPython/using_web_services_XML.py
+++ scientificComputingWithPython/using_web_services_XML.py
@@ -1,12 +1,42 @@
 # Wire Protocol
 # python Serialize  -> data -> De-Serialize java Hashmap
-# XML - help infomation systems share structured data
-# Standard Generalized Markup Language(SGML)
-# designed to be relatively human-legible
 
 
-# Tags
+# XML Schema Contract
 
-# Attributes
+# Description of the legal format of an document
 
-# Serialize / De-Serialize
+# XSD The W3C Schema specification for XML.
+
+import xml.etree.ElementTree as ET
+# data = '''<person>
+#     <name>Hudir</name>
+#     <phone type="intl">+ 49 1234567</phone>
+#     <email hide="yes"/>
+# </person>'''
+
+# tree = ET.fromstring(data)
+# print(tree)
+# print('Name', tree.find('name').text)
+# print('Attr', tree.find('email').get('hide'))
+
+data2 = '''<data2>
+<people>
+    <person>
+        <name>Hudir</name>
+        <phone type="intl">+ 49 1234567</phone>
+        <email hide="yes"/>
+    </person>
+    <person>
+        <name>Hudir</name>
+        <phone type="intl">+ 49 1234567</phone>
+        <email hide="yes"/>
+    </person>
+</people>
+</data2>'''
+
+tree2 = ET.fromstring(data2)
+lst = tree2.findall('people/person')
+for item in lst:
+    print('Phone', item.find('phone').text)
+    print('Attr', item.find('email').get('hide'))
