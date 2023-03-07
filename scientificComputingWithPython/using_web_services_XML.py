718b3e9adabe759b2aee0b8ecc7a87f1f21be252
Zhuo <hudirybw@gmail.com>
2023-03-07 14:49:47 +0100

    xml

---
+++ scientificComputingWithPython/using_web_services_XML.py
@@ -0,0 +1,12 @@
+# Wire Protocol
+# python Serialize  -> data -> De-Serialize java Hashmap
+# XML - help infomation systems share structured data
+# Standard Generalized Markup Language(SGML)
+# designed to be relatively human-legible
+
+
+# Tags
+
+# Attributes
+
+# Serialize / De-Serialize
