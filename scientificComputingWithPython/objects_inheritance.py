5bbe0783c1b084771dd458b68ad7a45ceb8fe8ff
Zhuo <hudirybw@gmail.com>
2023-03-09 16:42:06 +0100

    nice

---
+++ scientificComputingWithPython/objects_inheritance.py
@@ -0,0 +1,28 @@
+# subclassing - code reuse
+
+class PartyAnimal:
+    x=0
+    name = ''
+
+    def __init__(self, nam):
+        self.name = nam
+        print(self.name, 'constructed')
+
+    def party(self):
+        self.x = self.x + 1
+        print(self.name, "party count", self.x) 
+
+class FootballFan(PartyAnimal):
+    points = 0
+    def touchdown(self):
+        self.points = self.points + 7
+        self.party()
+        print(self.name, "pointts",self.points
+              )
+
+s = PartyAnimal("Sally")
+s.party()
+
+j = FootballFan("Jim")
+j.party()
+j.touchdown()
