aaeb6b355b445723adfc83a2b20c142454bb54e0
Zhuo <hudirybw@gmail.com>
2023-03-19 15:35:34 +0100

    nice

--- scientificComputingWithPython/pageRank/crawler.py
+++ scientificComputingWithPython/pageRank/crawler.py
@@ -1,18 +1,23 @@
 import urllib.request, urllib.parse, urllib.error
 import sqlite3
-import bs4
+from bs4 import BeautifulSoup 
 
 
 conn = sqlite3.connect('./pageRank/spider.sqlite')
 cur = conn.cursor()
 # Create Table and set columns V
-# ask for url
-# check db to see is that domin we can do this
+# ask for url V
+# check db to see is that domin we can do this V
+# while loop
+# take a not retrived page
 # check if we alread have html data of the url
 # if not, visite it get html and parse to find all a tags
 # check db, do we already have those Link or not
 # if not, add them to to do list
 # save the html, update the from-to table
+def updatePageError(conn, cur, url) :  
+    cur.execute('UPDATE Pages SET error = 1 WHERE url = ?', (url,))
+    conn.commit()
 
 cur.execute('CREATE TABLE IF NOT EXISTS Pages (id INTEGER PRIMARY KEY, url TEXT UNIQUE, html TEXT, error INTEGER, old_rank REAL, new_rank REAL)')
 cur.execute('CREATE TABLE IF NOT EXISTS Links (from_id INTEGER, to_id INTEGER)')
@@ -51,8 +56,64 @@ else:
     
 if len(web) > 1 :
         cur.execute('INSERT OR IGNORE INTO Webs (url) VALUES (?)', (web,))
-        cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_rank) VALUES (?, Null, 1.0)', (starturl, ))
+        cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_rank, error) VALUES (?, Null, 1.0, Null)', (starturl, ))
         conn.commit()
+
+count = 0
+while True:
+    if count < 1:
+          sval = input('How many pages: ')
+          if len(sval) < 1: break
+          count = int(sval)
+    count=count -1
+
+    cur.execute('SELECT id, url FROM Pages WHERE html is Null AND error is NULL ORDER BY RANDOM() LIMIT 1')
+    
+    try:
+          data = cur.fetchone()
+          from_id = data[0]
+          url = data[1]
+    except:
+        #   update error
+        updatePageError(conn,cur,url)
+        print(url, 'can not retrive')
+        continue
+
+    
+    cur.execute('DELETE FROM Links WHERE from_id = ? ', (from_id, ))
+
+    # call the url
+    try: 
+        doc = urllib.request.urlopen(url)
+        html = doc.read()
+        if doc.getcode() != 200:
+            print('Page Error', doc.getcode())
+            updatePageError(conn,cur,url)
+            continue
+
+               #   update error
+        if doc.info().get_content_type() != 'text/html' :
+             print('Page type ignore')
+             updatePageError(conn,cur,url)
+             continue
+        
+        print('(' + str(len(html)) + ')', end=' ')
+
+        print(html)
+        soup = BeautifulSoup(html, "html.parser")
+     
+             #   update error
+    except KeyboardInterrupt:
+         print('')
+         print('Stop Program by User')
+    except:
+         print('Unable to retrive or parse page')
+         updatePageError(conn,cur,url)
+         continue
+    
+    # till here all good 
+
+
 
 
 
