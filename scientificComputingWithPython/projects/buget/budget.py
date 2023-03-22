29bbeba693c4a9ecb79c3d64367118b46cbcde22
Zhuo <hudirybw@gmail.com>
2023-03-22 12:35:34 +0100

    all test passed

--- scientificComputingWithPython/projects/buget/budget.py
+++ scientificComputingWithPython/projects/buget/budget.py
@@ -1,13 +1,9 @@
 class Category:
-  title = None
-  funds = None
-  ledger = None
-
   def __init__(self, title):
      self.title = title
      self.ledger = list()
      self.funds = float()
-     print(self.title, 'constructed')
+   #   print(self.title, 'constructed')
 
   def deposit(self, *args):
     self.funds = self.funds + float(args[0])
@@ -38,8 +34,10 @@ class Category:
         budget.deposit(amount, "Transfer from " + self.title)
         return True
      return False
-  
   def get_balance(self):
+     return self.funds
+  
+  def __str__(self):
      import math
      namelen = len(self.title)
      left = '*' * math.floor((30 - namelen) / 2)
@@ -65,19 +63,19 @@ class Category:
      return line
      
 
-food = Category('food')
-food.deposit(100, 'sell drinks')
-food.withdraw(2, 'bonbon')
-food.withdraw(20, 'bonbon')
+# food = Category('food')
+# food.deposit(100, 'sell drinks')
+# food.withdraw(2, 'bonbon')
+# food.withdraw(20, 'bonbon')
 
-auto = Category('Auto')
-auto.deposit(100, 'sell drinks')
-auto.withdraw(2, 'bonbon')
-auto.withdraw(20, 'bonbon')
+# auto = Category('Auto')
+# auto.deposit(100, 'sell drinks')
+# auto.withdraw(2, 'bonbon')
+# auto.withdraw(20, 'bonbon')
 
-entertainment = Category('entertainment')
-entertainment.deposit(500, "sell pc")
-entertainment.transfer(100, food)
+# entertainment = Category('entertainment')
+# entertainment.deposit(500, "sell pc")
+# entertainment.transfer(100, food)
 
 # food.get_balance()
 # entertainment.get_balance()
@@ -88,7 +86,6 @@ def create_spend_chart(categories):
     totspent = 0
     maxlentitle = None
     for category in categories:
-       print(category)
        if (maxlentitle is None) or maxlentitle < len(category.title): 
           maxlentitle = len(category.title)
        record[category.title] = 0
@@ -98,7 +95,6 @@ def create_spend_chart(categories):
               totspent = totspent + abs(ledger["amount"])
 
     for category in list(record.items()):
-       print(category)
        record[category[0]] = (record[category[0]] / totspent * 100)
 
     line = "Percentage spent by category\n"
@@ -106,27 +102,30 @@ def create_spend_chart(categories):
     while count > -1:
        num = str(count)
        while len(num) < 3 : num = ' ' + num
-       line = line + num + '|'
+       line = line + num + '| '
        for category in list(record.items()):
           if category[1] > count:
-             line = line + ' ' + 'o'
+             line = line  + 'o' + '  '
+          else : line = line + '   '
        line = line + '\n'
        count = count -10
     
-    line = line + ' ' * 4 + '-'* (len(record) * 2 + 2) 
+    line = line + ' ' * 4 + '-'* (len(record) * 3 + 1)   + "\n"
 
     for index in range(maxlentitle):
-       line = line + "\n"
-       line = line + ' ' * 4
+       if not index == 0 :line = line + "  \n"
+       line = line + ' ' * 3
        for category in list(record.items()):
           if len(category[0]) > index:
-             line = line + ' ' + category[0][index]
-          else : line = line + '  '
+             line = line + '  ' + category[0][index]
+          else : line = line + '   '
+
+    line = line+'  '
        
     # print(line)
     return line
 
 
-create_spend_chart([food, entertainment])     
+# create_spend_chart([food, entertainment])     
 
-print(create_spend_chart([food, entertainment, auto]))
+# print(create_spend_chart([food, entertainment, auto]))
