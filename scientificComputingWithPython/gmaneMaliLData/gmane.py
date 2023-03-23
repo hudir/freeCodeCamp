7c105c81ad4dd7d5ac9986be5b04c2991ea20ed3
Zhuo <hudirybw@gmail.com>
2023-03-23 22:46:52 +0100

    wow

--- scientificComputingWithPython/gmaneMaliLData/gmane.py
+++ scientificComputingWithPython/gmaneMaliLData/gmane.py
@@ -6,7 +6,57 @@ from urllib.parse import urljoin
 from urllib.parse import urlparse
 import re
 from datetime import datetime, timedelta
-import dateutil.parser as parser
+
+
+# Not all systems have this so conditionally define parser
+try:
+    import dateutil.parser as parser
+except:
+    pass
+
+def parsemaildate(md) :
+    # See if we have dateutil
+    try:
+        pdate = parser.parse(tdate)
+        test_at = pdate.isoformat()
+        return test_at
+    except:
+        pass
+
+    # Non-dateutil version - we try our best
+
+    pieces = md.split()
+    notz = " ".join(pieces[:4]).strip()
+
+    # Try a bunch of format variations - strptime() is *lame*
+    dnotz = None
+    for form in [ '%d %b %Y %H:%M:%S', '%d %b %Y %H:%M:%S',
+        '%d %b %Y %H:%M', '%d %b %Y %H:%M', '%d %b %y %H:%M:%S',
+        '%d %b %y %H:%M:%S', '%d %b %y %H:%M', '%d %b %y %H:%M' ] :
+        try:
+            dnotz = datetime.strptime(notz, form)
+            break
+        except:
+            continue
+
+    if dnotz is None :
+        # print 'Bad Date:',md
+        return None
+
+    iso = dnotz.isoformat()
+
+    tz = "+0000"
+    try:
+        tz = pieces[4]
+        ival = int(tz) # Only want numeric timezone values
+        if tz == '-0000' : tz = '+0000'
+        tzh = tz[:3]
+        tzm = tz[3:]
+        tz = tzh+":"+tzm
+    except:
+        pass
+
+    return iso+tz
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -17,7 +67,7 @@ cur = conn.cursor()
 
 baseurl = "http://mbox.dr-chuck.net/sakai.devel/"
 
-cur.execute('''CREATE TABLE IF NOT EXISTS Messages (id, INTEGER UNIQUE, email TEXT, sent_at TEXT, subject TEXT, headers TEXT, body TEXT)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS Messages (id INTEGER UNIQUE, email TEXT, sent_at TEXT, subject TEXT, headers TEXT, body TEXT)''')
 
 # get current id of db
 start = None
@@ -72,5 +122,63 @@ while True:
     print(url, len(text))
     count = count + 1
 
+    # handle bad messages
     if not text.startswith("From "):
-        print()
+        print(text)
+        print("Did not find From")
+        fail = fail + 1
+        if fail > 5: break
+        continue
+
+    pos = text.find("\n\n")
+    if pos > 0 :
+        hdr = text[:pos]
+        body = text[pos + 2 :]
+    else:
+        print(text)
+        print("Could not find break between headers and body")
+        fail = fail + 1
+        if fail > 5: break
+        continue
+    
+    email = None
+    x = re.findall('\nFrom: .* <(\S+@\S+)>\n', hdr)
+    if len(x) == 1 :
+        email = x[0]
+        email = email.strip().lower()
+        email = email.replace("<","")
+    else:
+        x = re.findall('\nFrom: (\S+@\S+)\n', hdr)
+        if len(x) == 1 :
+            email = x[0]
+            email = email.strip().lower()
+            email = email.replace("<","")
+    
+    date = None
+    y = re.findall('\Date: .*, (.*)\n', hdr)
+    if len(y) == 1 :
+        tdate = y[0]
+        tdate = tdate[:26]
+        try: 
+            sent_at = parsemaildate(tdate)
+        except:
+            print(text)
+            print("Date parse fail", tdate)
+            fail = fail + 1
+            if fail > 5: break
+            continue
+
+    subject = None
+    z = re.findall('\Subject: (.*)\n', hdr)
+    if len(z) == 1 : subject = z[0].strip().lower()
+
+    # reset the fail counter
+    fail = 0
+    print("   ", email, sent_at, subject)
+    cur.execute('INSERT OR IGNORE INTO Messages (id, email, sent_at, subject, headers, body) VALUES (?, ?, ?, ?, ?, ?)', (start, email, sent_at, subject, hdr, body))
+    if count % 50 == 0 : conn.commit()
+    if count % 100 == 0 : time.sleep(1)
+
+cur.close()
+        
+            
