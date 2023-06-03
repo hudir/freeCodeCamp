81168db2f69303b4c7185174fbe0b1ce8ba4935f
Zhuo <hudirybw@gmail.com>
2023-06-03 14:54:02 +0200

    str2

--- coding_interview_prep/leetcode_basic_string.js
+++ coding_interview_prep/leetcode_basic_string.js
@@ -6,3 +6,14 @@ var reverseString = function(s) {
     }
     s.splice(0, len-1)
 };
+
+//  Reverse Integer
+var reverse = function(x) {
+    // 1 to string, 2 split 3 revers, 4 join, 5 add minus
+  
+    const minusZero = x < 0
+    x = +Math.abs(x).toString().split('').reverse().join('')
+    if (x > 0x7FFFFFFF) return 0
+    if(minusZero) x=0-x
+    return x
+};
