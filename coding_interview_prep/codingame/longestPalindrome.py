cfd0d05f4a789887512d0550ef81fcff6996e674
Zhuo <hudirybw@gmail.com>
2023-06-29 22:54:56 +0200

    miao

--- coding_interview_prep/codingame/longestPalindrome.py
+++ coding_interview_prep/codingame/longestPalindrome.py
@@ -3,7 +3,7 @@
 # the standard input according to the problem statement.
 
 # s = input()
-s= "bcacb"
+s= "CGTCATTCATGCGGAAGGGGTAAGACCATTAGAAGTAGGGATAGTCCCGAACCTCACTTACCACTCCCAATAAGGGATCCCTGTCTGAAGGATGAGTGTCAGCCAGTGTAACCCGATGAGGTACCCAGAAGCCGAACTGGGCCAGACAACCCGGCGCTAACGCACTCAAAGCCGGGACGCGACGCGACATATCGGCTAAGAGTAGGCCGGGAGTGTAGACCTTTGGGGTTGAATAAATCTGTCGTAGTAACCGGCT"
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
@@ -44,20 +44,36 @@ def sliceStringBasedOnList(arr):
             strToCheck = s[arr[x]:arr[y]+1]
             # print(strToCheck)
             if check(strToCheck) is True:
-                print(strToCheck)
+                # print(strToCheck)
                 res.append(strToCheck)
     return res
             
 
 
 checked = {}
-res = list()
+res = {}
+maxLen=-1
 
 for j in range(len(s)):
     if s[j] not in checked:
         checked[s[j]] = True
         posiList = getAllIndexes(s[j], s, j)
+        # print(posiList)
         if len(posiList) > 1:
-            # slice the string and check
             palindromeStr = sliceStringBasedOnList(posiList)
-print(palindromeStr)
+            if len(palindromeStr) > 0:
+                for strToCheck in palindromeStr:
+                    length = len(strToCheck) 
+                    if length >=maxLen and check(strToCheck):
+                        if length in res:
+                            res[length].append(strToCheck)
+                        else:
+                            res[length] = [strToCheck]
+                        if length > maxLen:
+                            maxLen = length
+
+for want in res[max(res.keys())]:
+    print(want)
+
+# print(res[index])
+# print(maxLen)
